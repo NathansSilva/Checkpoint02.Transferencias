06be6b2731a3cdb31e4dfc87a4345830aa8efefa
Nathan <Nathansilva18@outlook.com>
2022-10-02 20:58:18 -0300

    Adicionar arquivos de projeto.

---
+++ Checkpoint02.Transferencias/Controllers/TransferenciaController.cs
@@ -0,0 +1,76 @@
+﻿using Checkpoint02.Transferencias.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checkpoint02.Transferencias.Controllers
+{
+    public class TransferenciaController : Controller
+    {
+        private static List<Transferencia> _lista = new List<Transferencia>();
+
+        private static int _id = 0;
+
+        //Remove
+        [HttpPost]
+        public IActionResult Remover(Transferencia transferencia)
+        {
+
+            var index = _lista.FindIndex(c => c.Codigo == transferencia.Codigo);
+            _lista.RemoveAt(transferencia.Codigo);
+            TempData["msg"] = "Jogador removido!";
+            return RedirectToAction("Index");
+        }
+
+        //Edit
+        [HttpPost]
+        public IActionResult Editar(Transferencia transferencia)
+        {
+            var index = _lista.FindIndex(c => c.Codigo == transferencia.Codigo);
+            _lista[index] = transferencia;
+            TempData["msg"] = "Jogador atualizado";
+            return RedirectToAction("Index");
+        }
+
+        //Edit
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var transferencia = _lista.Find(match => match.Codigo == id);
+            return View(transferencia);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_lista);
+        }
+
+        //CRUD Cadastrar
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Transferencia transferencia)
+        {
+            transferencia.Codigo = ++_id;
+            _lista.Add(transferencia);
+            TempData["cadastro"] = "Jogador cadastrado";
+            return RedirectToAction("Cadastrar");
+        }
+
+        //Pesquisar
+        [HttpPost]
+        public IActionResult Pesquisar(string Nome)
+        {
+            var transferencia = _lista.Find(c => c.Nome == Nome);
+            return View(transferencia);
+        }
+
+        [HttpGet]
+        public IActionResult Pesquisar()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Checkpoint02.Transferencias/Models/Transferencia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Checkpoint02.Transferencias.Models
+{
+    public class Transferencia
+    {
+        [HiddenInput]
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Valor { get; set; }
+        public string? Origem { get; set; }
+        public string? Destino { get; set; }
+        public Posicao? Posicao { get; set; }
+        [Display(Name = "Data da Venda")]
+        public DateTime DataVenda { get; set; }
+        [Display(Name = "Definitivo")]
+        public bool Definitivo { get; set; }
+    }
+    public enum Posicao
+    {
+        Goleiro, Defesa, [Display(Name = "Meio Campo")] MeioCampo, Ataque
+    }
+}
